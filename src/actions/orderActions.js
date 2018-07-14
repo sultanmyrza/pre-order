aade353eaabb01bda3acd55fedb4df80dc0e422d
sultan <sultanmyrza@gmail.com>
2018-07-14 17:50:31 +0900

    add order actions

---
+++ src/actions/orderActions.js
@@ -0,0 +1,35 @@
+import { SET_ORDER_TYPE, SET_ORDER_TIME, SET_ORDER_TABLE, ORDER_ADD_ITEM, ORDER_REMOVE_ITEM, CANCEL_ORDER, CHANGE_ORDER_TYPE } from "./orderActionTypes";
+
+export const setOrderType = orderType => ({
+    type: SET_ORDER_TYPE,
+    orderType
+  });
+
+export const setOrderTime = orderTime => ({
+  type: SET_ORDER_TIME,
+  orderTime
+});
+
+export const setOrderTable = orderTable => ({
+  type: SET_ORDER_TABLE,
+  orderTable
+});
+
+export const orderAddItem = item => ({
+  type: ORDER_ADD_ITEM,
+  item
+});
+
+export const orderRemoveItem = item => ({
+  type: ORDER_REMOVE_ITEM,
+  item
+});
+
+export const cancelOrder = () => ({
+  type: CANCEL_ORDER
+});
+
+export const changeOrderType = orderType => ({
+  type: CHANGE_ORDER_TYPE,
+  orderType
+})
