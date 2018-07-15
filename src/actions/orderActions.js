abcc0074dcb699f7379309df7cd737e25b50a8d8
sultan <sultanmyrza@gmail.com>
2018-07-15 13:17:18 +0900

    pass all tests

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,35 +1,43 @@
-import { SET_ORDER_TYPE, SET_ORDER_TIME, SET_ORDER_TABLE, ORDER_ADD_ITEM, ORDER_REMOVE_ITEM, CANCEL_ORDER, CHANGE_ORDER_TYPE } from "./orderActionTypes";
+import {
+  SET_ORDER_TYPE,
+  SET_ORDER_TIME,
+  SET_ORDER_TABLE,
+  ORDER_ADD_ITEM,
+  ORDER_REMOVE_ITEM,
+  CANCEL_ORDER,
+  CHANGE_ORDER_TYPE,
+} from './orderActionTypes';
 
 export const setOrderType = orderType => ({
-    type: SET_ORDER_TYPE,
-    orderType
-  });
+  type: SET_ORDER_TYPE,
+  orderType,
+});
 
 export const setOrderTime = orderTime => ({
   type: SET_ORDER_TIME,
-  orderTime
+  orderTime,
 });
 
 export const setOrderTable = orderTable => ({
   type: SET_ORDER_TABLE,
-  orderTable
+  orderTable,
 });
 
 export const orderAddItem = item => ({
   type: ORDER_ADD_ITEM,
-  item
+  item,
 });
 
 export const orderRemoveItem = item => ({
   type: ORDER_REMOVE_ITEM,
-  item
+  item,
 });
 
 export const cancelOrder = () => ({
-  type: CANCEL_ORDER
+  type: CANCEL_ORDER,
 });
 
 export const changeOrderType = orderType => ({
   type: CHANGE_ORDER_TYPE,
-  orderType
-})
+  orderType,
+});
--- src/reducers/order.js
+++ src/reducers/order.js
@@ -1,13 +1,91 @@
-import { SET_ORDER_TYPE } from '../actions/orderActionTypes';
+import {
+  SET_ORDER_TYPE,
+  SET_ORDER_TIME,
+  SET_ORDER_TABLE,
+  ORDER_ADD_ITEM,
+  ORDER_REMOVE_ITEM,
+  CANCEL_ORDER,
+  CHANGE_ORDER_TYPE,
+} from '../actions/orderActionTypes';
 
-export default (order = (state = {}, action) => {
+function order(state = undefined, action) {
+  switch (action.type) {
+    case ORDER_ADD_ITEM:
+      if (state === undefined) {
+        return {
+          ...action.item,
+          quantity: 1,
+        };
+      } else {
+        return {
+          ...state,
+          id: action.item.id,
+          quantity: state.quantity + 1,
+        };
+      }
+    case ORDER_REMOVE_ITEM:
+      if (state === undefined || state.quantity <= 1) {
+        return undefined;
+      } else {
+        return {
+          ...state,
+          quantity: state.quantity - 1,
+        };
+      }
+    default:
+      return state;
+  }
+}
+
+export default function orders(state = {}, action) {
+  let newState;
+  let itemId;
   switch (action.type) {
     case SET_ORDER_TYPE:
       return {
         ...state,
         type: action.orderType,
       };
+    case SET_ORDER_TIME:
+      return {
+        ...state,
+        time: action.time,
+      };
+    case SET_ORDER_TABLE:
+      return {
+        ...state,
+        tables: action.tables,
+      };
+    case ORDER_ADD_ITEM:
+      itemId = action.item.id;
+      return {
+        ...state,
+        itemsByIds: {
+          ...state.itemsByIds,
+          [itemId]: order(state.itemsByIds[itemId], action),
+        },
+      };
+    case ORDER_REMOVE_ITEM:
+      itemId = action.item.id;
+      return {
+        ...state,
+        itemsByIds: {
+          ...state.itemsByIds,
+          [itemId]: order(state.itemsByIds[action.item.id], action),
+        },
+      };
+    case CANCEL_ORDER:
+      return {};
+    case CHANGE_ORDER_TYPE:
+      newState = { ...state, type: action.orderType };
+      if (action.orderType === 'pickup') {
+        delete newState.tables;
+        return newState;
+      } else {
+        newState.tables = 1;
+        return newState;
+      }
     default:
       return state;
   }
-});
+}
