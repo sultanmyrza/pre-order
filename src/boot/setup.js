62bd2bcacad52816fef082d4ae087bc2e489de1d
sultanmyrza <sultanmyrza@sultanmyrzas-iMac.local>
2018-07-16 09:57:06 +0900

    cache images before app loads

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import * as Expo from 'expo';
+import { AppLoading, Asset, Font } from 'expo';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { Image } from 'react-native';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger'
+import logger from 'redux-logger';
 import RootNavigationStack from '../navigation/RootNavigation';
 import reducer from '../reducers';
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(logger)));
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
 
 export default class Setup extends Component {
   constructor() {
@@ -17,18 +32,29 @@ export default class Setup extends Component {
     };
   }
 
-  componentWillMount() {
-    this.loadFonts();
-  }
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('../../assets/images/products/hamburger.jpg'),
+      require('../../assets/images/products/pizza.jpg'),
+      require('../../assets/images/products/popcorn.jpg'),
+      require('../../assets/images/products/ramen.jpg'),
+      require('../../assets/images/products/steak.jpg'),
+    ]);
 
-  async loadFonts() {
-    await Expo.Font.loadAsync({});
-    this.setState({ isReady: true });
+    const fontAssets = cacheFonts([{}]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
   }
 
   render() {
     if (!this.state.isReady) {
-      return <Expo.AppLoading />;
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
     }
     return (
       <Provider store={store}>
