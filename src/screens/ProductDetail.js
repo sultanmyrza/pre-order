1e2fe0691d464147efa8a9279cb53060fc57568b
sultanmyrza <sultanmyrza@sultanmyrzas-iMac.local>
2018-07-16 09:49:07 +0900

    make product detail page scrollable

--- src/screens/ProductDetail.js
+++ src/screens/ProductDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import images from '../../assets/images';
 
 class ProductDetail extends Component {
@@ -18,31 +18,33 @@ class ProductDetail extends Component {
   render() {
     const { product } = this.props.navigation.state.params;
     return (
-      <View style={{ flex: 1, backgroundColor: 'white' }}>
-        <View style={{ flex: 2 }}>
-          <View style={{ flex: 4, padding: 30 }}>
-            <Image
-              style={{ flex: 1, width: null, height: null }}
-              source={images[product.category]}
-            />
+      <ScrollView contentContainerStyle={{ flex: 1 }}>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+          <View style={{ flex: 2 }}>
+            <View style={{ flex: 4, padding: 30 }}>
+              <Image
+                style={{ flex: 1, width: null, height: null }}
+                source={images[product.category]}
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <Text style={{ fontSize: 24, fontWeight: 'bold', alignSelf: 'center' }}>
+                {product.price} won
+              </Text>
+            </View>
           </View>
-          <View style={{ flex: 1 }}>
-            <Text style={{ fontSize: 24, fontWeight: 'bold', alignSelf: 'center' }}>
-              {product.price} won
+
+          <View style={{ flex: 1, padding: 30 }}>
+            <Text style={{ fontSize: 18, alignSelf: 'center' }}>
+              Food is any substance[1] consumed to provide nutritional support for an organism. It
+              is usually of plant or animal origin, and contains essential nutrients, such as
+              carbohydrates, fats, proteins, vitamins, or minerals. The substance is ingested by an
+              organism and assimilated by the organism's cells to provide energy, maintain life, or
+              stimulate growth.
             </Text>
           </View>
         </View>
-
-        <View style={{ flex: 1, padding: 30 }}>
-          <Text style={{ fontSize: 18, alignSelf: 'center' }}>
-            Food is any substance[1] consumed to provide nutritional support for an organism. It is
-            usually of plant or animal origin, and contains essential nutrients, such as
-            carbohydrates, fats, proteins, vitamins, or minerals. The substance is ingested by an
-            organism and assimilated by the organism's cells to provide energy, maintain life, or
-            stimulate growth.
-          </Text>
-        </View>
-      </View>
+      </ScrollView>
     );
   }
 }
